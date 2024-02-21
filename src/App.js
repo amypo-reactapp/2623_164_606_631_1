a961de9378f6782ea759d4697403a61ed4fab1ac
amypo-reactapp <reactapp@amypo.in>
2024-02-21 15:03:45 +0000

    User

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 function App() {
   return (
     <div>
+
      <h1><img src='https://w7.pngwing.com/pngs/235/872/png-transparent-react-computer-icons-redux-javascript-others-logo-symmetry-nodejs-thumbnail.png'></img></h1>
+     <h6>React Skill Session</h6>
      <p>React. js is an open-source JavaScript library, crafted with precision by Facebook, that aims to simplify the intricate process of building interactive user interfaces. Imagine a user interface built with React as a collection of components, each responsible for outputting a small, reusable piece of HTML code</p>
 
     </div>
